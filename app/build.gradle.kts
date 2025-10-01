0be23ab720c67572a7a45ba0ac6c4f2c846522db
Marco Salvi <marco.salvi@ingv.it>
2025-10-01 10:11:57 +0200

    update version and strings

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
 		applicationId = "it.ingv.finitesource"
 		minSdk = 26
 		targetSdk = 36
-		versionCode = 3
-		versionName = "1.2"
+		versionCode = 4
+		versionName = "1.3"
 
 		testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 	}
