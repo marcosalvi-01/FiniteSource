2f6afa82afb2205905c70872cab45d0316bbf42b
Marco Salvi <marco.salvi@ingv.it>
2025-10-24 15:45:05 +0200

    update dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
 		applicationId = "it.ingv.finitesource"
 		minSdk = 26
 		targetSdk = 36
-		versionCode = 5
-		versionName = "1.4"
+		versionCode = 6
+		versionName = "1.5"
 
 		testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 	}
@@ -47,50 +47,50 @@ android {
 
 dependencies {
 	// room
-	val roomVersion = "2.6.1"
+	val roomVersion = "2.8.2"
 	implementation("androidx.room:room-runtime:$roomVersion")
 	implementation("androidx.room:room-ktx:$roomVersion")
 	annotationProcessor("androidx.room:room-compiler:$roomVersion")
 	ksp("androidx.room:room-compiler:$roomVersion")
 
 	// hilt
-	val hiltVersion = "2.48.1"
+	val hiltVersion = "2.57.2"
 	implementation("com.google.dagger:hilt-android:$hiltVersion")
 	ksp("com.google.dagger:hilt-android-compiler:$hiltVersion")
 
 	// lifecycle
-	val lifecycleVersion = "2.6.2"
+	val lifecycleVersion = "2.9.4"
 	implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:$lifecycleVersion")
 	implementation("androidx.lifecycle:lifecycle-livedata-ktx:$lifecycleVersion")
 	implementation("androidx.lifecycle:lifecycle-runtime-ktx:$lifecycleVersion")
 
 	// OpenAPI generator
-	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:1.9.10")
-	implementation("org.jetbrains.kotlin:kotlin-reflect:1.9.10")
-	implementation("com.squareup.moshi:moshi-kotlin:1.15.0")
-	implementation("com.squareup.moshi:moshi-adapters:1.15.0")
-	ksp("com.squareup.moshi:moshi-kotlin-codegen:1.15.0")
-	implementation("com.squareup.okhttp3:logging-interceptor:4.12.0")
-	implementation("com.squareup.retrofit2:retrofit:2.9.0")
-	implementation("com.squareup.retrofit2:converter-moshi:2.9.0")
-	implementation("com.squareup.retrofit2:converter-scalars:2.9.0")
+	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:2.2.0")
+	implementation("org.jetbrains.kotlin:kotlin-reflect:2.2.0")
+	implementation("com.squareup.moshi:moshi-kotlin:1.15.2")
+	implementation("com.squareup.moshi:moshi-adapters:1.15.2")
+	ksp("com.squareup.moshi:moshi-kotlin-codegen:1.15.2")
+	implementation("com.squareup.okhttp3:logging-interceptor:5.2.1")
+	implementation("com.squareup.retrofit2:retrofit:3.0.0")
+	implementation("com.squareup.retrofit2:converter-moshi:3.0.0")
+	implementation("com.squareup.retrofit2:converter-scalars:3.0.0")
 	testImplementation("io.kotlintest:kotlintest-runner-junit5:3.4.2")
 
 	// other
-	implementation("androidx.core:core-ktx:1.12.0")
-	implementation("androidx.appcompat:appcompat:1.6.1")
-	implementation("com.google.android.material:material:1.11.0")
-	implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-	implementation("androidx.activity:activity-ktx:1.8.2")
-	implementation("androidx.fragment:fragment-ktx:1.6.2")
-	implementation("androidx.datastore:datastore-preferences:1.0.0")
-	implementation("androidx.datastore:datastore:1.0.0")
+	implementation("androidx.core:core-ktx:1.17.0")
+	implementation("androidx.appcompat:appcompat:1.7.1")
+	implementation("com.google.android.material:material:1.13.0")
+	implementation("androidx.constraintlayout:constraintlayout:2.2.1")
+	implementation("androidx.activity:activity-ktx:1.11.0")
+	implementation("androidx.fragment:fragment-ktx:1.8.9")
+	implementation("androidx.datastore:datastore-preferences:1.1.7")
+	implementation("androidx.datastore:datastore:1.1.7")
 	implementation("com.davemorrissey.labs:subsampling-scale-image-view-androidx:3.10.0")
 	implementation("com.google.android.flexbox:flexbox:3.0.0")
-	implementation("com.github.bumptech.glide:glide:4.15.1")
-	annotationProcessor("com.github.bumptech.glide:compiler:4.15.1")
-	implementation("org.osmdroid:osmdroid-android:6.1.17")
-	implementation("com.opencsv:opencsv:5.5.2")
+	implementation("com.github.bumptech.glide:glide:5.0.5")
+	annotationProcessor("com.github.bumptech.glide:compiler:5.0.5")
+	implementation("org.osmdroid:osmdroid-android:6.1.20")
+	implementation("com.opencsv:opencsv:5.12.0")
 
 
 	// testing
