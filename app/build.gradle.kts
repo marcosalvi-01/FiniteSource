2998875c02cfbb341a92746b5bb746df898ea495
Marco Salvi <marco.salvi@ingv.it>
2025-12-10 10:38:42 +0100

    update version to 1.6

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
 		applicationId = "it.ingv.finitesource"
 		minSdk = 26
 		targetSdk = 36
-		versionCode = 6
-		versionName = "1.5"
+		versionCode = 7
+		versionName = "1.6"
 
 		testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 	}
