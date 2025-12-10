788e3458a8bcb30c1729c25306514cc28347a757
Marco Salvi <marco.salvi@ingv.it>
2025-12-10 10:29:07 +0100

    fix: use correct scenario directory name

--- app/src/main/java/it/ingv/finitesource/data/remote/ApiCalls.kt
+++ app/src/main/java/it/ingv/finitesource/data/remote/ApiCalls.kt
@@ -113,7 +113,7 @@ class ApiCalls @Inject constructor(
             val scenarios: MutableList<Scenario> = mutableListOf()
             for (scenarioType in availableScenarios) {
                 for (scenario in event.scenarios!!.providerDetails) {
-                    if (scenario.providerName!! == scenarioType.name) {
+                    if (scenario.providerName!! == scenarioType.dir) {
                         for (product in scenario.products!!) {
                             if (product.focalPlane!!.intValueExact() == focalPlaneType.ordinal + 1) {
                                 val displacementMapDescription =
